d23d5a4b29e03a3ab6987f154b36cee3ca4f716b
Eslam Ahmed Refaat <ieslamvx@gmail.com>
2024-11-28 20:16:16 +0200

    Add files via upload

---
+++ EslamVx.h
@@ -0,0 +1,79 @@
+// EslamVx.h
+#pragma once
+// Input/Output
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+// General Utilities
+#include <utility>
+#include <functional>
+// Data Structures
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+// Algorithms and Iterators
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+// Math and Random Numbers
+#include <cmath>
+#include <complex>
+#include <random>
+#include <cstdlib>
+// Strings and Character Handling
+#include <string>
+#include <cstring>
+#include <cctype>
+// Time and Date
+#include <chrono>
+#include <ctime>
+// Error Handling
+#include <exception>
+#include <stdexcept>
+#include <system_error>
+// Multithreading
+#include <thread>
+#include <mutex>
+#include <future>
+#include <condition_variable>
+// Memory Management
+#include <memory>
+#include <scoped_allocator>
+// Localization
+#include <locale>
+#include <codecvt> // Deprecated in C++20
+// Type Support
+#include <typeinfo>
+#include <type_traits>
+// Miscellaneous
+#include <bitset>
+#include <tuple>
+#include <variant>
+#include <optional>
+#include <any>
+#include <span>
+#include <limits>
+#include <cassert>
+#include <valarray>
+#include <initializer_list>
+#include <cstdio>
+#include <climits>
+#include <cfloat>
+#include <cwchar>
+#include <cwctype>
+#include <cfenv>
+#include <regex>
+#include <filesystem>
+#include <locale>
+//EslamVx speedcode
+#define EslamVx std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+#define Arbic std::locale::global(std::locale("ar-EG.UTF-8")); std::wcout.imbue(std::locale("ar-EG.UTF-8"));
